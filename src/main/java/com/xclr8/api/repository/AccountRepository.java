9c44c78e3e2d306352b1eda259958075629d79e6
BigabuBeaze <munpan475@hotmail.com>
2018-06-27 00:50:10 +0800

    Tests for specific retrieval of accounts

--- src/main/java/com/xclr8/api/repository/AccountRepository.java
+++ src/main/java/com/xclr8/api/repository/AccountRepository.java
@@ -3,5 +3,11 @@ package com.xclr8.api.repository;
 import com.xclr8.api.model.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository extends MongoRepository<Account, String> {
+    Account findOneByFirstName(String firstName);
+
+    List<Account> findAllByFirstName(String firstName);
 }
--- src/main/java/com/xclr8/api/web/controller/AccountController.java
+++ src/main/java/com/xclr8/api/web/controller/AccountController.java
@@ -1,11 +1,15 @@
 package com.xclr8.api.web.controller;
 
+import com.xclr8.api.model.Account;
+import com.xclr8.api.repository.AccountRepository;
 import com.xclr8.api.service.AccountService;
 import com.xclr8.api.web.response.AccountResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/account")
@@ -14,8 +18,22 @@ public class AccountController {
     @Autowired
     AccountService mAccountService;
 
+    @Autowired
+    AccountRepository mAR;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
     public Iterable<AccountResponse> accounts() {
         return mAccountService.findAllAccounts();
+    }
+
+
+    @RequestMapping(value = "/1", method = RequestMethod.GET)
+    public Account existTest(String name) {
+        return mAR.findOneByFirstName(name);
+    }
+
+    @RequestMapping(value = "/2", method = RequestMethod.GET)
+    public List<Account> existsTest(String name) {
+        return mAR.findAllByFirstName(name);
     }
 }
