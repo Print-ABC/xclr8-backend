41331982acaf32de77fa4b7db9be516494f28aad
BigabuBeaze <munpan475@hotmail.com>
2018-07-05 13:42:07 +0800

    Small fix to session api

--- src/main/java/com/xclr8/api/nestedModel/Exercise.java
+++ src/main/java/com/xclr8/api/nestedModel/Exercise.java
@@ -1,7 +1,5 @@
 package com.xclr8.api.nestedModel;
 
-import com.xclr8.api.nestedModel.Exercise_Config;
-import com.xclr8.api.nestedModel.Exercise_Result;
 import lombok.Data;
 
 @Data
@@ -10,10 +8,10 @@ public class Exercise {
      Exercise object to be stored in the Session objects in the database
      */
 
-    int exerciseId;
+    String exerciseId;
     String exerciseName;
-    Exercise_Config config;
-    Exercise_Result result;
+    ExerciseConfig config;
+    ExerciseResult result;
     String bodyPart;
     //0 = Not completed, 1 = Completed.
     int status;
--- src/main/java/com/xclr8/api/nestedModel/ExerciseConfig.java
+++ src/main/java/com/xclr8/api/nestedModel/ExerciseConfig.java
@@ -3,9 +3,9 @@ package com.xclr8.api.nestedModel;
 import lombok.Data;
 
 @Data
-public class Exercise_Config {
+public class ExerciseConfig {
     /**
-     Exercise_Config objects to be stored in the Exercise objects that are to be stored in the Session objects in the database
+     ExerciseConfig objects to be stored in the Exercise objects that are to be stored in the Session objects in the database
      */
 
     String sides;
