5f2fb0e0e9844e392373b6ea84422813b4c924c5
Tan Suan Khai <mail.abcwxyz@gmail.com>
2018-07-21 16:56:37 +0800

    Remove unused imports

--- src/main/java/com/xclr8/api/repository/NotificationRepository.java
+++ src/main/java/com/xclr8/api/repository/NotificationRepository.java
@@ -3,7 +3,6 @@ package com.xclr8.api.repository;
 import com.xclr8.api.model.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
--- src/main/java/com/xclr8/api/web/request/SessionExerciseRequest.java
+++ src/main/java/com/xclr8/api/web/request/SessionExerciseRequest.java
@@ -1,7 +1,6 @@
 package com.xclr8.api.web.request;
 
 import com.xclr8.api.model.nestedModel.Exercise;
-import com.xclr8.api.model.nestedModel.Summary;
 import lombok.Data;
 
 import java.util.List;
